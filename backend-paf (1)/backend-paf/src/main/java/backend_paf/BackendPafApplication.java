c58d5e51a7b94f3a13871a8078b778b685ac1586
Jananga07 <janangachandima07@gmail.com>
2026-03-31 01:09:58 +0530

    backend/frontend setup

---
+++ backend-paf (1)/backend-paf/src/main/java/backend_paf/BackendPafApplication.java
@@ -0,0 +1,13 @@
+package backend_paf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPafApplication.class, args);
+	}
+
+}
